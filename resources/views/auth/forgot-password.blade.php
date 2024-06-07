3e47856a8648f062a9a975f2ea6dd1a225084b6b
daffisclr <daffisclr@gmail.com>
2024-06-07 16:46:00 +0700

    back to email

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,12 +9,11 @@
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address or Username -->
+        <!-- Email Address -->
         <div>
-            <x-input-label for="input_type" :value="__('Email or Username')" />
-            <x-text-input id="input_type" class="block mt-1 w-full" type="text" name="input_type" :value="old('input_type')" required autofocus />
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
