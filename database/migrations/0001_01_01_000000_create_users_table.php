fc8b59744be738d5fa88a5568352d73fcea7e2e9
daffisclr <daffisclr@gmail.com>
2024-06-07 14:26:43 +0700

    add user (alumni + admin) database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->string('phone_number')->nullable();
+            $table->enum('role',['admin','alumni'])->default('alumni');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
